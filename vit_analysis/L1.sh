68800525700451df9d607f84d8e3fcc00508d2ce
jgoldfeder <ygoldfed@gmail.com>
2025-04-04 12:14:22 -0400

    updated gammas

--- vit_analysis/L1.sh
+++ vit_analysis/L1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # device
-bash finetune_vit.sh $1 L1 25000
-bash finetune_vit.sh $1 L1 30000
-bash finetune_vit.sh $1 L1 35000
-bash finetune_vit.sh $1 L1 40000
-bash finetune_vit.sh $1 L1 45000 
-bash finetune_vit.sh $1 L1 50000 
-bash finetune_vit.sh $1 L1 55000 
-bash finetune_vit.sh $1 L1 60000 
-bash finetune_vit.sh $1 L1 65000 
+bash finetune_vit.sh $1 L1 50
+bash finetune_vit.sh $1 L1 200
+bash finetune_vit.sh $1 L1 500
+bash finetune_vit.sh $1 L1 1000
+bash finetune_vit.sh $1 L1 2000 
+bash finetune_vit.sh $1 L1 4000 
+bash finetune_vit.sh $1 L1 5000 
+bash finetune_vit.sh $1 L1 10000 
+bash finetune_vit.sh $1 L1 20000 
